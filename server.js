4ceba57bdd1dc031ff96716c9692543f409c4017
Ricky199X <rickyrojas19@gmail.com>
2020-07-28 18:00:55 -0400

    added routes for user, articles, sites

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 
+// Initialize the users routes - any request to api/users will go to the users.js file
+const users = require('./routes/api/users')
+const articles = require('./routes/api/articles')
+const sites = require('./routes/api/sites')
+
+
+// Use Routes
+app.use('./api/users', users)
+app.use('./api/articles', articles)
+app.use('./api/sites', sites)
+
+
 // initialize express 
 const app = express()
 
@@ -22,3 +34,5 @@ mongoose
 // We may deploy to heroku - so we need to set the env variable 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`server started on port ${port}`))
+
+
