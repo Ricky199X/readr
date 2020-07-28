cc56d095ac4b431fac6fc705d7130e7ca4ad1f50
Ricky199X <rickyrojas19@gmail.com>
2020-07-28 17:39:13 -0400

    added user model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// create schema - this is an object literal
+// each user has a username and password 
+
+const UserScehma = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// need to export the model so that the database can have access to it 
+module.exports = User = mongoose.model('user', UserSchema)
